13c1644bef74c8b97ed0af38fc37a989a142a4da
ashiqul50021 <ashiqulislamrasel@gmail.com>
2021-10-19 20:05:14 +0600

    booking doctors

--- src/components/HomePage/DoctorsBooked/DoctorsBooked/DoctorsBooked.js
+++ src/components/HomePage/DoctorsBooked/DoctorsBooked/DoctorsBooked.js
@@ -3,36 +3,50 @@ import { useParams } from 'react-router';
 // import DoctorsDetails from '../../Doctorslist/DoctorsDetails';
 
 const DoctorsBooked = () => {
-    const {doctorsId } = useParams();
-    // const [doctorsDetails, setDoctorDetails] = useState([]);
-    // console.log(doctorsId);
+    const { doctorsId } = useParams();
 
-    const url = `../../../../../public/doctors.json`;
-    useEffect(()=> {
-        fetch(url)
-        .then(res => res.json())
-        .then(data => console.log(data))
-    },[])
+    const [doctorsuser, setDoctoruser] = useState([]);
+    const [singledoctor, setSingleDoctor] = useState({});
 
-    // const search = doctorsDetails.find(details => details.id === doctorsId)
-    // console.log(search)
-    // console.log(props)
-    
+    useEffect(() => {
+        fetch("/doctors.json")
+            .then(res => res.json())
+            .then(data => setDoctoruser(data))
+    }, [])
 
-  
+    useEffect(() => {
+        if (doctorsuser.length) {
+            const singledoctors = doctorsuser.find(doctor => doctor.id === parseInt(doctorsId))
+            setSingleDoctor(singledoctors)
+        }
+    }, [doctorsId, setSingleDoctor, doctorsuser])
 
 
- 
+
+
+
 
 
 
 
     return (
-        <div>
-            <h2>This is booking: {doctorsId} </h2>
-         
-            
-           
+        <div class="card text-center">
+            <div class="card-header">
+               <h3>Doctor Details</h3>
+            </div>
+            <div class="card-body mt-3">
+                <img src={singledoctor.img} alt="" />
+                <h5 class="card-title mt-5">Doctors Name: {singledoctor.name}</h5>
+                <small>{singledoctor.title}</small>
+                <p class="card-text">{singledoctor.description}</p>
+                <p>Phone: {singledoctor.phone}</p>
+                <p>visit fee: {singledoctor.fee}</p>
+                <p>Duty: {singledoctor.shift}</p>
+                
+            </div>
+            <div class="card-footer text-muted">
+               <button className="btn btn-secondary">Booking Confirm</button>
+            </div>
         </div>
     );
 };
--- src/components/HomePage/Doctorslist/DoctorsDetails.js
+++ src/components/HomePage/Doctorslist/DoctorsDetails.js
@@ -3,31 +3,27 @@ import useAuth from '../../../hooks/useAuth';
 
 const DoctorsDetails = (props) => {
     const { user, logout } = useAuth();
-    const {id, name, title, img, description, fee, phone , shift} = props.doctorslist;
+    const { id, name, title, img, description, fee, phone, shift } = props.doctorslist;
     return (
         <div class="col">
-        <div class="card h-100  shadow p-3 mb-5 bg-body rounded border-0">
-            <img src={img} class="card-img-top" alt="..." height="200px" />
-            <div class="card-body">
-                <h5 class="card-title">{name}</h5>
-                <small style={{color:'blue', marginBottom:'10px'}}>{title}</small>
-                <p> {description}</p>
+            <div class="card h-100  shadow p-3 mb-5 bg-body rounded border-0">
+                <img src={img} class="card-img-top" alt="..." height="200px" />
+                <div class="card-body">
+                    <h5 class="card-title">{name}</h5>
+                    <small style={{ color: 'blue', marginBottom: '10px' }}>{title}</small>
+                    <p> {description}</p>
 
-                
-                <p>phone : {phone}</p>
-                <p>fee : {fee}</p>
-                <p>shift: {shift}</p>
-                <a class="btn btn-dark" href={`/doctorsBooked/${id}`} role="button">Book {name}</a>
+                    <a class="btn btn-dark" href={`/doctorsBooked/${id}`} role="button">Book {name}</a>
                 </div>
 
-                
-
-                
 
 
-            
+
+
+
+
+            </div>
         </div>
-    </div>
     );
 };
 
